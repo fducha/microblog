856cbce398f2641fb4290a4668c0f8713efd7c04
fducha <fducha@list.ru>
2018-05-07 20:20:40 +0300

    chap 5 is done !!!!

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from app import application
+from app import application, db
 from app.forms import LoginForm
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user
 from app.models import User
+from werkzeug.urls import url_parse
+from app.forms import RegistrationForm
 
 
 @application.route('/')
@@ -50,7 +52,10 @@ def login():
             flash(u'Неправильное имя пользователя или пароль')
             return redirect(url_for('login'))
         login_user(user=user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', form=form, title='Sing In')
 
 
@@ -58,3 +63,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@application.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(u'Поздравляем, пользователь {} <{}> зарегистрирован!!'.format(user.username, user.email))
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form, title=u'Регистрация')
